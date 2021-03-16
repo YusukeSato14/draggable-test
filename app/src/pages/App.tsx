b832a298bda425cdbcfd31ec07bdad4b2fbbb4ba
YusukeSato14 <usk.sugar14@gmail.com>
2021-03-16 22:00:17 +0900

    カード個々に色つけ

--- app/src/pages/App.tsx
+++ app/src/pages/App.tsx
@@ -4,21 +4,34 @@ import '../assets/App.css';
 import CardList from '../components/CardList';
 
 function App() {
-  const [cards, setCards] = useState([{ id: 0 }]);
+  const initialCardState = {
+    id: 0,
+    deltaPosition: {
+      x: 0,
+      y: 0,
+    },
+    style: { backgroundColor: '#000000' }
+  };
+  const [cards, setCards] = useState([initialCardState]);
   const addCard: MouseEventHandler = () => {
     setCards([
       ...cards,
       {
         id: cards.length,
+        deltaPosition: {
+          x: 0,
+          y: 0,
+        },
+        style: { backgroundColor: '#000000' },
       }
     ]);
   };
   return (
     <div className="App">
       <button className="button" onClick={addCard}>add</button>
-      <CardList cards={cards} />
+      <CardList cards={cards} setCards={setCards} />
     </div>
   );
-}
+};
 
 export default App;
