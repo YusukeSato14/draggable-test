28eaed5a13dd947bd3193a518c40d84a80ff55a1
YusukeSato14 <usk.sugar14@gmail.com>
2021-03-18 20:49:09 +0900

    add 色固定機能

--- app/src/components/CardList.tsx
+++ app/src/components/CardList.tsx
@@ -1,4 +1,5 @@
-import { InputBase } from '@material-ui/core';
+import { createStyles, InputBase, Switch, Theme, withStyles } from '@material-ui/core';
+import { ChangeEvent, useState } from 'react';
 import Draggable, { DraggableData, DraggableEvent } from 'react-draggable';
 
 type Props = {
@@ -40,11 +41,53 @@ export const colorList = [
   '#BA68C8',
   '#F06292',
   '#E57373',
-]
+];
+
+const AntSwitch = withStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      width: 28,
+      height: 16,
+      padding: 2,
+      display: 'flex',
+      float: 'right',
+    },
+    switchBase: {
+      padding: 4,
+      color: theme.palette.grey[500],
+      '&$checked': {
+        transform: 'translateX(10px)',
+        color: theme.palette.common.white,
+        '& + $track': {
+          opacity: 1,
+          backgroundColor: '#80DEEA',
+          borderColor: '#E0E0E0',
+        },
+      },
+    },
+    thumb: {
+      width: 10,
+      height: 10,
+      boxShadow: 'none',
+    },
+    track: {
+      border: `1px solid #E0E0E0`,
+      borderRadius: 16 / 2,
+      opacity: 1,
+      backgroundColor: theme.palette.common.white,
+    },
+    checked: {},
+  }),
+)(Switch);
 
 const CardList = ({ cards, setCards }: Props) => {
+  const [fixedColor, setFixedColor] = useState(false);
+  const handleColor = (event: ChangeEvent<HTMLInputElement>) => {
+    setFixedColor(!fixedColor);
+  };
+
   const handleDrag = (id: number) => (e: DraggableEvent, ui: DraggableData) => {
-    const styles = transformColor(id, cards[id].deltaPosition.x, cards[id].deltaPosition.y);
+    const styles = fixedColor ? cards[id].style : transformColor(id, cards[id].deltaPosition.x, cards[id].deltaPosition.y);
 
     let { x, y } = cards[id].deltaPosition;
     setCards([
@@ -59,7 +102,7 @@ const CardList = ({ cards, setCards }: Props) => {
         value: cards[id].value,
       },
       ...cards.slice(id + 1)
-    ])
+    ]);
   };
 
   const transformColor = (id: number, x: number, y: number) => {
@@ -71,7 +114,7 @@ const CardList = ({ cards, setCards }: Props) => {
     return styles;
   };
 
-  const handleChange = (id: number) => (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleChange = (id: number) => (event: ChangeEvent<HTMLInputElement>) => {
     setCards([
       ...cards.slice(0, id),
       {
@@ -81,14 +124,19 @@ const CardList = ({ cards, setCards }: Props) => {
         value: event.target.value,
       },
       ...cards.slice(id + 1)
-    ])
-  }
+    ]);
+  };
 
   return (
     <div className="center">
       {cards.map(card => (
         <Draggable bounds="body" onDrag={handleDrag(card.id)} key={card.id}>
           <div key={card.id} className="paper" style={card.style}>
+            <AntSwitch
+              checked={fixedColor}
+              onChange={handleColor}
+              name="colorSwitch"
+            />
             <InputBase
               key={card.id}
               className="paper"
