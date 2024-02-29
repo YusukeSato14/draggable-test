df1998889ed1c05b129ac66df137eac3c69a41f3
YusukeSato14 <usk.sugar14@gmail.com>
2024-02-29 18:06:14 +0000

    NextAuthの対応

--- app/app/[[...slug]]/client.tsx
+++ app/app/[[...slug]]/client.tsx
@@ -1,10 +1,18 @@
 "use client";
 
+import { Auth0Provider } from "@auth0/auth0-react";
 import dynamic from "next/dynamic";
 import React from "react";
 
 const App = dynamic(() => import("../../src/pages/App"), { ssr: false });
 
 export function ClientOnly() {
-  return <App />;
+  return (
+    <Auth0Provider
+      domain={process.env.NEXT_PUBLIC_AUTH0_DOMAIN ?? ""}
+      clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID ?? ""}
+    >
+      <App />;
+    </Auth0Provider>
+  );
 }
